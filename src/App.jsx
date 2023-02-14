0f8941a8912488e196095f4bb3cf3512d130279f
Alex R. Farnes <alexrfarnes@gmail.com>
2023-02-14 14:27:10 +0800

    Create a loader for the careers and an Error element.

--- src/App.jsx
+++ src/App.jsx
@@ -15,17 +15,38 @@ import Contact from './pages/help/Contact';
 import RootLayout from './layouts/RootLayout';
 import HelpLayout from './layouts/HelpLayout';
 import NotFound from './pages/NotFound';
+import CareersLayout from './layouts/CareersLayout';
+import Careers, { careersLoader } from './pages/careers/Careers';
+import CareerDetails, {
+  careerDetailsLoader,
+} from './pages/careers/CareerDetails';
+import CareersError from './pages/careers/CareersError';
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path='/' element={<RootLayout />}>
         <Route index element={<Home />} />
+
         <Route path='about' element={<About />} />
+
         <Route path='help' element={<HelpLayout />}>
           <Route path='faq' element={<Faq />} />
           <Route path='contact' element={<Contact />} />
         </Route>
+
+        <Route
+          path='careers'
+          element={<CareersLayout />}
+          errorElement={<CareersError />}>
+          <Route index element={<Careers />} loader={careersLoader} />
+          <Route
+            path=':id'
+            element={<CareerDetails />}
+            loader={careerDetailsLoader}
+          />
+        </Route>
+
         <Route path='*' element={<NotFound />} />
       </Route>
     )
